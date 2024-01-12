6f268aca6a9fcb8c7bbc85b2f746129bef041cea
DevCassemiroPT <alexandre.140395@gmail.com>
2024-01-12 10:52:44 +0000

    Trabalhando com Arrays

--- array.js
+++ array.js
@@ -4,6 +4,13 @@ let max = Math.max(...array);
 console.log(max); 
 */
 
-var nomes = [Fábio, Manuel, Felipe, Lucas, Igor]
+var nomes = ["Fábio", "Manuel", "Felipe", "Lucas", "Igor"]
 var alteracao1 = nomes.splice(0,1, "Alexandre")
+
+//Adc um novo elemento
+nomes.unshift("Marcia")
 console.log(nomes)
+
+//Localizando um elemento
+var index = nomes.indexOf("Lucas");
+console.log(index)
