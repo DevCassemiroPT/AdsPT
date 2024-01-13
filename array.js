b22e7f7d5812767e8050efdd0ad591ca28dc255b
DevCassemiroPT <alexandre.140395@gmail.com>
2024-01-13 17:24:25 +0000

    Adc um novo arquivo de array

--- array.js
+++ array.js
@@ -17,14 +17,34 @@ console.log(index);
 */
 
 //Avançando 
+/*
 var pessoas = ["Felipe", "Igor", "Alexandre", "Cassemiro","Teodoro", "Nadir"];
 var pessoas1 = pessoas.slice(1,4);
-/*
+
 console.log(pessoas);
 console.log(pessoas1);
 */
 
 //Concatenando
+/*
 var gerentes = ["Nadia", "Fabiana"];
 var empresa = pessoas1.concat(gerentes);
 console.log(empresa);
+*/
+
+//Exercício - criando um array trimestral
+var meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
+             "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+
+var Ptrimestre = meses.slice(0,3);
+console.log("\nTrismestres de 2024\n")
+console.log("1º: " + Ptrimestre + "!\n")
+
+var Strimestre = meses.slice(3,6)
+console.log("2º: " + Strimestre + "!\n")
+
+var Ttrimestre = meses.slice(6,9)
+console.log("3º: " + Ttrimestre + "!\n")
+
+var Qtrimestre = meses.slice(9,12)
+console.log("4º: " + Qtrimestre + "!\n")
