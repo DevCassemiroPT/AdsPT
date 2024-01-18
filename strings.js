5dffe7b5ce387ce3ef345b9664a6220c5a42cef3
DevCassemiroPT <alexandre.140395@gmail.com>
2024-01-18 21:17:53 +0000

    Trabalhando com strings

---
+++ strings.js
@@ -0,0 +1,7 @@
+var nomeCompleto = "Igor";
+var tamanho = nomeCompleto.length;
+
+console.log(tamanho);
+
+resultado = tamanho <= 7 ? "Por favor digite seu nome completo" : "Seja bem vindo(a)";
+console.log(resultado);
