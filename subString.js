78d774f8a53e76fcfc07e00f7e6d07883d1ca7ae
DevCassemiroPT <alexandre.140395@gmail.com>
2024-01-23 18:07:04 +0000

    Trabalhando com funções

--- subString.js
+++ subString.js
@@ -13,11 +13,34 @@ if(frase.startsWith("Olá")){
     console.log(novaStringMaiuscula);
 }
 
-//Exercícios - exemplo do professor 
+ 
+//Exercícios 
+// 1º split, startWith, substring, for, if e toUpperCase
 var url = "http://minhaempresa.com.br/filme?nome=poder&atores=Lucas,Guilherme";
 const parametros = url.split("?")[1];
 console.log("1º etapa » " + parametros);
 
 const valores = parametros.split("&");
 console.log("\n2º etapa » " + valores);
+
+for(let i = 0; i < valores.length; i++){
+    if(valores[i].startsWith("atores=")){
+        valores[i] = "atores=" + valores[i].substring(7).toUpperCase()
+    }
+};
+console.log("\n3º etapa » " + valores + "\n\n");
+
+
+//2º - arrays, map, length, if e toUpperCase
+let nomes = ["Bernardo", "Ana", "Luizinho", "João", "Cida", "Sabrina"];
+
+let nomeTranformados = nomes.map((nome) => {
+     if(nome.length < 5){
+        return nome.toUpperCase();
+    } else {
+        return nome;
+    }
+});
+console.log(nomeTranformados);
+
 
