4b6e57f1148ea3691ce85ee562594a78aeed1e04
DevCassemiroPT <alexandre.140395@gmail.com>
2024-01-27 16:30:13 +0000

    Exercício prático de herença e classe

--- ExerHerancaeClasses.js
+++ ExerHerancaeClasses.js
@@ -1,48 +1,48 @@
 class Funcionario{
-    constructor(nome,idade,cargo){
+    constructor(nome, idade, cargo){
         this.nome = nome;
         this.idade = idade;
         this.cargo = cargo;
-        this.trabalhando = false;
-    }
-    seApresentar(){
-        console.log("Olá prazer, eu me chamo")
-    }
-    trabalhar(){
-        if(this.trabalhando){
-            console.log("Está trabalhando");
-            return;
-    }
-    this.trabalhando = true;
     }
 
+    seApresentar(){
+        console.log(`Olá, meu nome é ${this.nome}, tenho ${this.idade} anos e sou ${this.cargo}.`)
+    }
+
+    trabalhar() {
+        console.log(`${this.nome} está trabalhando.`);
+    }  
 }
 
 class Gerente extends Funcionario{
-    constructor(departamento){
-        this.departamento = departamento;
-        this.gerenciamento = false;
+    constructor(nome, idade, cargo, departamento){
+        super(nome, idade, cargo)
+        this.departamento = departamento;     
     }
 
     gerenciar(){
-        if(this.gerenciamento){
-            console.log("Gerenciamento realizado");
-            return;
-        }
-        this.gerenciamento = true;
+        console.log(`${this.nome} está gerenciando o departamento de ${this.departamento}.`);
     }
 }
 
 class Desenvolvedor extends Funcionario{
-    constructor(linguagem){
+    constructor(nome, idade, cargo, linguagem){
+        super(nome, idade, cargo)
         this.linguagem = linguagem;
-        this.programando = false;
     }
+
     programar(){
-        if(this.programando){
-            console.log("Está programando");
-            return;
-        }
-        this.programando = true;
+            console.log(`${this.nome} está desenvolvendo em ${this.linguagem}.`);  
     }
 }
+
+let gerente = new Gerente(`Igor`, 28, `gerente`, `projetos`);
+let desenvolvedor = new Desenvolvedor(`Alexandre`, 30, `Developer FullStack`, `JavaScript`);
+
+gerente.seApresentar();
+gerente.trabalhar();
+gerente.gerenciar();
+
+desenvolvedor.seApresentar();
+desenvolvedor.trabalhar();
+desenvolvedor.programar();
